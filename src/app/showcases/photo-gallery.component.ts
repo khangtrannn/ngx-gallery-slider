85054ff5859516be609b63370a8b8170e2775713
Khang Tran <khangtrann8198@gmail.com>
2024-06-16 23:43:44 +0700

    update

--- src/app/showcases/photo-gallery.component.ts
+++ src/app/showcases/photo-gallery.component.ts
@@ -6,12 +6,10 @@ import { GallerySlideComponent } from "../gallery-slide/gallery-slide.component"
   selector: 'app-photo-gallery',
   template: `
     <ngx-gallery-carousel [padding]="'1rem'" [gradientBackground]="false">
-      <ngx-gallery-slide header="Gallery" image="images/khang1.jpeg" />
-      <ngx-gallery-slide header="Gallery" image="images/khang2.jpeg" />
       <ngx-gallery-slide header="Gallery" image="images/khang3.jpeg" />
+      <ngx-gallery-slide header="Gallery" image="images/khang2.jpeg" />
+      <ngx-gallery-slide header="Gallery" image="images/khang1.jpeg" />
       <ngx-gallery-slide header="Gallery" image="images/khang4.jpeg" />
-      <ngx-gallery-slide header="Gallery" image="images/khang5.jpeg" />
-      <ngx-gallery-slide header="Gallery" image="images/khang6.jpeg" />
     </ngx-gallery-carousel>
   `,
   standalone: true,
